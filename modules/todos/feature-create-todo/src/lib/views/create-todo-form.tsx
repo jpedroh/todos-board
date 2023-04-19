723334bf4f3fcf89f533eca2c79c39fd1165a830
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-04-19 20:53:33 -0300

    refactor: extract form into shared component

--- modules/todos/feature-create-todo/src/lib/views/create-todo-form.tsx
+++ modules/todos/feature-create-todo/src/lib/views/create-todo-form.tsx
@@ -2,54 +2,9 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import { CreateTodo, useCreateTodo } from '@todos-board/modules/todos/core';
-import { FormEvent, useState } from 'react';
-
-function TodoForm({ onSubmit }: { onSubmit: (todo: CreateTodo) => void }) {
-  function handleSubmit(evt: FormEvent) {
-    evt.preventDefault();
-    const data = Object.fromEntries(
-      new FormData(evt.target as HTMLFormElement)
-    );
-    onSubmit(data);
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            margin="dense"
-            fullWidth
-            name="title"
-            id="title"
-            label="Title"
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            margin="dense"
-            multiline
-            fullWidth
-            rows={4}
-            name="description"
-            id="description"
-            label="Description"
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button style={{ width: '100%' }} variant="contained">
-            Save
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
-  );
-}
+import { useCreateTodo } from '@todos-board/modules/todos/core';
+import { TodoForm } from '@todos-board/modules/todos/shared';
+import { useState } from 'react';
 
 export function CreateTodoForm() {
   const createTodo = useCreateTodo();
