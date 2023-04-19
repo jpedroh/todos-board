1da2eb7669b9472fafd89c13a128870d1d10a77a
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2023-04-19 20:50:15 -0300

    feat: style create todo feature with material ui

--- modules/todos/feature-create-todo/src/lib/views/create-todo-form.tsx
+++ modules/todos/feature-create-todo/src/lib/views/create-todo-form.tsx
@@ -1,26 +1,74 @@
-import { useCreateTodo } from '@todos-board/modules/todos/core';
-import { FormEvent } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import { CreateTodo, useCreateTodo } from '@todos-board/modules/todos/core';
+import { FormEvent, useState } from 'react';
 
-export function CreateTodoForm() {
-  const createTodo = useCreateTodo();
-
+function TodoForm({ onSubmit }: { onSubmit: (todo: CreateTodo) => void }) {
   function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
     const data = Object.fromEntries(
       new FormData(evt.target as HTMLFormElement)
     );
-    createTodo(data);
+    onSubmit(data);
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title</label>
-      <input required name="title" id="title" />
-
-      <label htmlFor="description">Description</label>
-      <textarea required name="description" id="description" />
-
-      <button>Create</button>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            margin="dense"
+            fullWidth
+            name="title"
+            id="title"
+            label="Title"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            margin="dense"
+            multiline
+            fullWidth
+            rows={4}
+            name="description"
+            id="description"
+            label="Description"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button style={{ width: '100%' }} variant="contained">
+            Save
+          </Button>
+        </Grid>
+      </Grid>
     </form>
+  );
+}
+
+export function CreateTodoForm() {
+  const createTodo = useCreateTodo();
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button variant="outlined" onClick={handleOpen}>
+        Add Todo
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Create a Todo</DialogTitle>
+        <DialogContent>
+          <TodoForm onSubmit={createTodo} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,18 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CreateTodoForm } from '@todos-board/modules/todos/feature-create-todo';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <CreateTodoForm />
-    </>
+    </ThemeProvider>
   );
 }
